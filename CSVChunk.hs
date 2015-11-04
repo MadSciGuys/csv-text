3b9588930b1bfb786424cd8e27275c45f94eb6e4
ilongsho <ilongsho@ITS.JNJ.com>
2015-11-04 13:52:04 -0500

    Now I've added a lines mapping function.

--- CSVChunk.hs
+++ CSVChunk.hs
@@ -1,5 +1,5 @@
 {-# OPTIONS_GHC -Wall #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE LambdaCase, OverloadedStrings #-}
 
 module CSVChunk where
 
@@ -14,7 +14,7 @@ import Data.ByteString.Builder (Builder, hPutBuilder, char8, lazyByteString)
 import Data.Int (Int64)
 import Data.Monoid ((<>))
 import System.Directory (removeFile)
-import System.IO (IOMode(WriteMode), withFile)
+import System.IO (IOMode(WriteMode), withFile, Handle)
 
 import Prelude hiding (head)
 
@@ -25,11 +25,25 @@ type BC = BC.ByteString
 type BLC = BLC.ByteString
 
 --I'm gonna see how well this monad-parallel library works.
-foldMapSplitFile :: (Monoid m) 
-  => Int64 -> FilePath -> (BC -> BC -> m) -> IO m
+mapLinesSplitFile :: Int64 -> FilePath -> Handle -> (BC -> BC -> BC) -> IO ()
+mapLinesSplitFile chunkSize inPath outHandle toBC = do
+  (hdr, chunks) <- splitFileIO chunkSize inPath
+  tmps <- flip MP.mapM chunks $ \ chunk -> do 
+    let tmp = (chunk++".tmp")
+    BC.writeFile tmp . BC.intercalate "\n" 
+      . map (toBC hdr) . BC.lines =<< BC.readFile chunk
+    removeFile chunk
+    return tmp
+  putOut hdr
+  flip mapM_ tmps $ \ tmp -> do
+    putOut =<< BC.readFile tmp
+    removeFile tmp
+  where putOut = BC.hPutStrLn outHandle
+
+foldMapSplitFile :: (Monoid m) => Int64 -> FilePath -> (BC -> BC -> m) -> IO m
 foldMapSplitFile chunkSize inPath toMonoid = withSplit chunkSize inPath
-  $ uncurry $ \ hdr -> fmap mconcat . MP.mapM
-    (fmap (foldMap (toMonoid hdr) . BC.lines) . BC.readFile)
+  $ uncurry $ \ hdr -> fmap mconcat 
+      . MP.mapM (fmap (foldMap (toMonoid hdr) . BC.lines) . BC.readFile)
 
 withSplit :: Int64 -> FilePath -> ((BC, [FilePath]) -> IO b) -> IO b
 withSplit chunkSize inPath 
